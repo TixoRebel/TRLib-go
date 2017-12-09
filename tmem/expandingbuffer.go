cbe043ff3a8cf7b087f889309dfab7daf499342d
Andrew Strauss <astrauss11@gmail.com>
2017-12-09 03:05:49 -0500

    Added ExpandingBuffer Length

--- tmem/expandingbuffer.go
+++ tmem/expandingbuffer.go
@@ -12,10 +12,11 @@ type node struct {
 type ExpandingBuffer struct {
 	head *node
 	tail *node
-	size int
+	nodeSize int
 	writeLock* sync.Mutex
 	readLock* sync.Mutex
 	wrote* sync.Cond
+	length int
 }
 
 func NewExpandingBuffer() (e *ExpandingBuffer) {
@@ -23,6 +24,7 @@ func NewExpandingBuffer() (e *ExpandingBuffer) {
 	e.writeLock = new(sync.Mutex)
 	e.readLock = new(sync.Mutex)
 	e.wrote = sync.NewCond(e.writeLock)
+	e.length = 0
 	return
 }
 
@@ -42,6 +44,7 @@ func (e *ExpandingBuffer) Write(data []byte) {
 
 		i := copy(e.tail.data[len(e.tail.data):cap(e.tail.data)], data)
 		data = data[i:]
+		e.length += i
 		e.tail.data = e.tail.data[:i + len(e.tail.data)]
 	}
 
@@ -71,6 +74,7 @@ func (e *ExpandingBuffer) WriteDirect(length int, read func([]byte) (int, error)
 		i, err := read(e.tail.data[len(e.tail.data):sz])
 
 		wrote += i
+		e.length += i
 
 		e.tail.data = e.tail.data[:i + len(e.tail.data)]
 
@@ -106,15 +110,16 @@ func (e *ExpandingBuffer) Read(data []byte) (read int) {
 		i := copy(data[read:], e.head.data)
 		e.head.data = e.head.data[i:]
 		read += i
+		e.length -= i
 	}
 
 	return
 }
 
 func (e *ExpandingBuffer) makeNode() (n *node) {
-	if e.size == 0 { e.size = 1024 }
+	if e.nodeSize == 0 { e.nodeSize = 100 }
 	n = new(node)
-	n.data = make([]byte, e.size)
+	n.data = make([]byte, e.nodeSize)
 	n.data = n.data[0:0]
 	
 	return
@@ -122,10 +127,14 @@ func (e *ExpandingBuffer) makeNode() (n *node) {
 
 func (e *ExpandingBuffer) SetNodeSize(size int) {
 	if size >= 0 {
-		e.size = size
+		e.nodeSize = size
 	}
 }
 
 func (e *ExpandingBuffer) GetNodeSize() int {
-	return e.size
+	return e.nodeSize
+}
+
+func (e *ExpandingBuffer) GetLength() int {
+	return e.length
 }
